f7840f1a8330d7fc40e152434bc6587906e1276f
Reena Mendonsa <reenamendonsa@gmail.com>
2021-11-10 12:59:16 +0530

    modification jset tests

--- block-BJaajc/code/index.js
+++ block-BJaajc/code/index.js
@@ -4,25 +4,26 @@ function getFullName(firstName, lastName) {
   }
   
   function palindrome(word) {
-    let Word = word.split('').reverse().join('');
-    if (Word === word) {
+    let newWord = word.split('').reverse().join('');
+    if (newWord === word) {
       return true;
     } else {
       return false;
     }
   }
   
-  function getCircumfrence() {
-    return `The Circumference is ${2 * Math.PI * radius}`;
+  function getCircumference(radius) {
+    return `The circumference is ${2 * Math.PI * radius}`;
   }
   
   function getArea(radius) {
-    return `The area is ${Math * radius * radius}`;
+    return `The area is ${Math.PI * radius * radius}`;
   }
+  
   
   module.exports = {
     getFullName,
     palindrome,
-    getCircumfrence,
+    getCircumference,
     getArea,
   };
--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -1,5 +1,5 @@
 const {
-    getCircumfrence,
+    getCircumference,
     getArea,
     getFullName,
     palindrome,
@@ -15,25 +15,28 @@ const {
   });
   
   test('test for circumference of the circle', () => {
-    expect(getCircumfrence(2)).toBe(`The circumference is 12.566370614359172`);
-    expect(getCircumfrence(3)).toBe(`The circumference is 18.84955592153876`);
-    expect(getCircumfrence(7)).not.toBe(`The circumference is 0`);
-    expect(getCircumfrence(8)).not.toBe(`The circumference is 19`);
-    
+    expect(getCircumference(2)).toBe(`The circumference is 12.566370614359172`);
+    expect(getCircumference(3)).toBe(`The circumference is 18.84955592153876`);
+    expect(getCircumference(4)).toBe(`The circumference is 25.132741228718345`);
+    expect(getCircumference(7)).not.toBe(`The circumference is 0`);
+    expect(getCircumference(8)).not.toBe(`The circumference is 19`);
+    expect(getCircumference(9)).not.toBe(`The circumference is 26`);
   });
   
   test('test for area of the circle', () => {
     expect(getArea(2)).toBe(`The area is 12.566370614359172`);
     expect(getArea(3)).toBe(`The area is 28.274333882308138`);
+    expect(getArea(4)).toBe(`The area is 50.26548245743669`);
     expect(getArea(7)).not.toBe(`The circumference is 0`);
     expect(getArea(8)).not.toBe(`The circumference is 19`);
-    
+    expect(getArea(9)).not.toBe(`The circumference is 26`);
   });
-  
+
+
   test('test for palindome', () => {
-    expect(palindrome('happy')).toBe(true);
-    expect(palindrome('kid')).toBe(true);
-    expect(palindrome('dog')).toBe(true);
+    expect(palindrome('anna')).toBe(true);
+    expect(palindrome('wow')).toBe(true);
+    expect(palindrome('mam')).toBe(true);
     expect(palindrome('sunshine')).not.toBe(true);
     expect(palindrome('art')).not.toBe(true);
     expect(palindrome('flowers')).not.toBe(true);
